1b457b56c9ad67ec68b66c7aabd3a47e049f6b0a
Wen Kokke <wenkokke@users.noreply.github.com>
2021-01-21 21:25:39 +0000

    Finished binary choice.

--- src/Control/Concurrent/Session/Linear.hs
+++ src/Control/Concurrent/Session/Linear.hs
@@ -8,7 +8,6 @@
 {-# LANGUAGE NoImplicitPrelude       #-}
 {-# LANGUAGE RankNTypes              #-}
 {-# LANGUAGE ScopedTypeVariables     #-}
-{-# LANGUAGE TypeApplications        #-}
 {-# LANGUAGE TypeFamilies            #-}
 {-# LANGUAGE TypeFamilyDependencies  #-}
 {-# LANGUAGE TypeOperators           #-}
@@ -22,7 +21,7 @@ import           Prelude.Linear hiding (Dual, IO)
 import qualified Control.Concurrent.Session.Raw.Linear as Raw
 import           Control.Monad.Linear
 import           Data.Kind (Type)
-import           GHC.TypeLits (Nat)
+import           GHC.TypeLits (Nat, type (+))
 import qualified GHC.TypeLits as Nat
 import qualified System.IO.Linear as Linear
 import           System.IO.Unsafe (unsafePerformIO)
@@ -34,9 +33,7 @@ data Priority
   | Top
 
 type family (p :: Priority) <=? (q :: Priority) :: Bool where
-  'Bot   <=? 'Bot   = 'True
-  'Bot   <=? 'Val m = 'True
-  'Bot   <=? 'Top   = 'True
+  'Bot   <=? p      = 'True
   'Val n <=? 'Bot   = 'False
   'Val n <=? 'Val m = n Nat.<=? m
   'Val n <=? 'Top   = 'True
@@ -63,36 +60,58 @@ data End  (o :: Nat)
 
 class ( Session (Dual s)
       , Dual (Dual s) ~ s
-      , Raw.Session (Repr s)
-      , Repr (Dual s) ~ Raw.Dual (Repr s)
+      , Raw.Session (Raw s)
+      , Raw (Dual s) ~ Raw.Dual (Raw s)
       ) => Session s where
-  type Dual s = result | result -> s
-  type Repr s
+  type Dual (s :: Type) = (result :: Type) | result -> s
+  type Pr   (s :: Type) :: Nat
+  type Raw  (s :: Type) :: Type
 
 instance Session s => Session (Send o a s) where
   type Dual (Send o a s) = Recv o a (Dual s)
-  type Repr (Send o a s) = Raw.Send a (Repr s)
+  type Pr   (Send o a s) = o
+  type Raw  (Send o a s) = Raw.Send a (Raw s)
 
 instance Session s => Session (Recv o a s) where
   type Dual (Recv o a s) = Send o a (Dual s)
-  type Repr (Recv o a s) = Raw.Recv a (Repr s)
+  type Pr   (Recv o a s) = o
+  type Raw  (Recv o a s) = Raw.Recv a (Raw s)
 
 instance Session (End o) where
   type Dual (End o) = End o
-  type Repr (End o) = Raw.End
+  type Pr   (End o) = o
+  type Raw  (End o) = Raw.End
 
-data Chan t s = (Session s) => Chan (Repr s)
+data Chan t s = (Session s) => Chan (Raw s)
 
-new :: Session s => Sesh t 'Top 'Bot (Chan t s, Chan t (Dual s))
-new = Sesh $ Raw.new >>= \(here, there) -> return (Chan here, Chan there)
+
+-- * Sesh monad
 
 ibind :: (q <= p') => (a %1 -> Sesh t p' q' b) %1 -> Sesh t p q a %1 -> Sesh t p q' b
 ibind mf mx = Sesh $ unSesh mx >>= (unSesh . mf)
 
-ireturn :: a -> Sesh t p p a
+infixr 1 =<<<
+infixl 1 >>>=
+
+(=<<<) :: (q <= p') => (a %1 -> Sesh t p' q' b) %1 -> Sesh t p q a %1 -> Sesh t p q' b
+(=<<<) mf mx = Sesh $ unSesh mx >>= (unSesh . mf)
+
+(>>>=) :: (q <= p') => Sesh t p q a %1 -> (a %1 -> Sesh t p' q' b) %1 -> Sesh t p q' b
+(>>>=) = flip ibind
+
+ireturn :: a %1 -> Sesh t p p a
 ireturn x = Sesh $ return x
 
-withNew :: Session s => ((Chan t s, Chan t (Dual s)) %1 -> Sesh t 'Top q a) -> Sesh t 'Top q a
+runSesh :: (forall t. Sesh t p q a) -> Linear.IO a
+runSesh mx = let (Sesh x) = mx in unsafePerformIO (Unsafe.coerce x)
+
+
+-- * Communication primitives
+
+new :: Session s => Sesh t 'Top 'Bot (Chan t s, Chan t (Dual s))
+new = Sesh $ Raw.new >>= \(here, there) -> return (Chan here, Chan there)
+
+withNew :: Session s => ((Chan t s, Chan t (Dual s)) %1 -> Sesh t 'Top q a) %1 -> Sesh t 'Top q a
 withNew mf = ibind mf new
 
 spawn :: Sesh t p q () %1 -> Sesh t 'Top 'Bot ()
@@ -107,8 +126,44 @@ recv (Chan ch) = Sesh $ Raw.recv ch >>= \(x, ch) -> return (x, Chan ch)
 close :: Chan t (End o) %1 -> Sesh t 'Top ('Val o) ()
 close (Chan ch) = Sesh $ Raw.close ch
 
-runSesh :: (forall t. Sesh t p q a) -> Linear.IO a
-runSesh mx = let (Sesh x) = mx in unsafePerformIO (Unsafe.coerce x)
+cancel :: Session s => Chan t s %1 -> Sesh t 'Top 'Bot ()
+cancel (Chan ch) = Sesh $ Raw.cancel ch
+
+
+-- * Binary choice
+
+type ChanSelect t o s1 s2
+  = Chan t (Send o (Either (Chan t (Dual s1)) (Chan t (Dual s2))) (End (o + 1)))
+
+type ChanOffer t o s1 s2
+  = Chan t (Recv o (Either (Chan t s1) (Chan t s2)) (End (o + 1)))
+
+selectLeft :: (Session s1, Session s2, Pr s1 ~ Pr s2) =>
+              ChanSelect t o s1 s2 %1 ->
+              Sesh t 'Top ('Val o) (Chan t s1)
+selectLeft (ch :: ChanSelect t o s1 s2) =
+  new >>>= \(here, there) ->
+  send (Left there, ch) >>>= \ch ->
+  cancel ch >>>= \() ->
+  ireturn here
+
+selectRight :: (Session s1, Session s2, Pr s1 ~ Pr s2) =>
+               ChanSelect t o s1 s2 %1 ->
+               Sesh t 'Top ('Val o) (Chan t s2)
+selectRight ch =
+  new >>>= \(here, there) ->
+  send (Right there, ch) >>>= \ch ->
+  cancel ch >>>= \() ->
+  ireturn here
+
+offerEither :: (Session s1, Session s2, 'Val o <= p) =>
+               (Either (Chan t s1) (Chan t s2) %1 -> Sesh t p q a) %1 ->
+               ChanOffer t o s1 s2 ->
+               Sesh t 'Top q a
+offerEither match ch =
+  recv ch >>>= \(x, ch) ->
+  cancel ch >>>= \() ->
+  match x
 
 -- -}
 -- -}
