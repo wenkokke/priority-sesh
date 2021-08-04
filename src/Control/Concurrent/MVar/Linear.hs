478964737ba8adcef1c84402fd5677aff0f6954d
Wen Kokke <wenkokke@users.noreply.github.com>
2021-08-04 23:45:39 +0100

    added module for linear week pointers

--- src/Control/Concurrent/MVar/Linear.hs
+++ src/Control/Concurrent/MVar/Linear.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE LinearTypes #-}
-
 module Control.Concurrent.MVar.Linear
   ( MVar
   , newEmptyMVar
@@ -7,24 +5,17 @@ module Control.Concurrent.MVar.Linear
   , putMVar
   ) where
 
-import           Prelude.Linear hiding (IO)
-import           Control.Concurrent.MVar (MVar)
-import qualified Control.Concurrent.MVar as MVar
-import           Data.Unrestricted.Linear
-import qualified System.IO.Linear as Linear
-import           Unsafe.Linear as Unsafe
+import Control.Concurrent.MVar (MVar)
+import Control.Concurrent.MVar qualified as MVar
+import Data.Unrestricted.Linear
+import System.IO.Linear qualified as Linear
+import Unsafe.Linear as Unsafe
 
 newEmptyMVar :: Linear.IO (Ur (MVar a))
-newEmptyMVar =
-  Linear.fromSystemIOU MVar.newEmptyMVar
+newEmptyMVar = Linear.fromSystemIOU MVar.newEmptyMVar
 
 takeMVar :: MVar a %1-> Linear.IO a
-takeMVar mvar =
-  Linear.fromSystemIO (Unsafe.toLinear MVar.takeMVar mvar)
+takeMVar mvar = Linear.fromSystemIO (Unsafe.toLinear MVar.takeMVar mvar)
 
 putMVar :: MVar a %1-> a %1-> Linear.IO ()
-putMVar mvar x =
-  Linear.fromSystemIO (Unsafe.toLinear2 MVar.putMVar mvar x)
-
-instance Consumable (MVar a) where
-  consume = Unsafe.toLinear (\_mvar -> ())
+putMVar mvar x = Linear.fromSystemIO (Unsafe.toLinear2 MVar.putMVar mvar x)
